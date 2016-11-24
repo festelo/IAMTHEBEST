465a9d9d329c599f350edf34a753ac0525a51821
Igor <igor.spiridonov.99@mail.ru>
2016-11-24 17:03:19 +0300

    Randomize

--- src/Test2/MainTest2.java
+++ src/Test2/MainTest2.java
@@ -12,7 +12,7 @@ public class MainTest2 extends Application {
 
     public MainTest2(Stage primaryStageTest2) throws IOException{
         Parent rootTest2 = FXMLLoader.load(getClass().getResource("test1.fxml"));
-        primaryStageTest2.setTitle("Test 2");
+        primaryStageTest2.setTitle("Second Test");
         primaryStageTest2.setScene(new Scene(rootTest2));
     }
 
